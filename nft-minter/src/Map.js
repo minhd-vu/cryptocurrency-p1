8154f3500abc9ec2bb6944798c86516d3900fb5c
Edwin <60202421+emarino135@users.noreply.github.com>
2022-05-16 16:06:05 -0400

    New Style

--- nft-minter/src/Map.js
+++ nft-minter/src/Map.js
@@ -63,7 +63,7 @@ const Map = (props) => {
         ref={mapRef}
         width="100%"
         height="100%"
-        mapStyle="mapbox://styles/mapbox/light-v9"
+        mapStyle="mapbox://styles/emarino135/cl395kp5v000715q5xkqsmi4h9"
         onViewportChange={handleViewportChange}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       >
