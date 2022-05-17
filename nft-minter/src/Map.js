24273f82221eff96c78527cea9649a8651975172
Minh Vu <minhd_vu@yahoo.com>
2022-05-16 23:47:17 -0400

    added new marker not rendering

--- nft-minter/src/Map.js
+++ nft-minter/src/Map.js
@@ -4,45 +4,13 @@ import { web3 } from "./util/interact";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const Pin = (props) => {
-  const { size = 50, onClick, image } = props;
-
-  return (
-    <img
-      src={image}
-      alt="Pin"
-      height={size}
-      viewBox="0 0 24 24"
-      style={{
-        cursor: "pointer",
-        transform: `translate(${-size / 2}px,${-size}px)`,
-      }}
-      onClick={onClick}
-    />
-  );
-};
-
 const Map = (props) => {
-
-  const [tokens, setTokens] = useState([]);
-
   const mapContainer = useRef(null);
   const map = useRef(null);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/emarino135/cl395mdm3001714qgcd7adwsz",
-      center: [-77.3074, 38.8298],
-      zoom: 8,
-    });
-  });
 
   // fetch all of the NFTs from the contract with location metadata
   useEffect(() => {
     async function init() {
-      const tokens = [];
       let tokenId = 3669;
       let response;
       do {
@@ -51,12 +19,39 @@ const Map = (props) => {
           tokenId: tokenId++,
         });
         if (response.metadata.location) {
-          tokens.push(response.metadata);
+          const coords = response.metadata.location;
+          // add the marker here
+
+          console.log(response.metadata);
+
+          const size = 50;
+          const el = document.createElement("img");
+          el.src = response.metadata.image;
+          el.style.height = `${size}px`;
+          el.style.width = `${size}px`;
+          el.style.cursor = "pointer";
+          el.style.backgroundSize = "cover";
+          el.style.backgroundImage = `url(${response.metadata.image})`;
+          el.style.transform = `translate(${-size / 2}px,${-size}px)`;
+          el.viewBox = "0 0 24 24";
+
+          console.log(el);
+
+          new mapboxgl.Marker(el)
+            .setLngLat([coords.longitude, coords.latitude])
+            .addTo(map.current);
         }
       } while (!response.error);
-
-      setTokens(tokens);
     }
+
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/emarino135/cl395mdm3001714qgcd7adwsz",
+      center: [-77.3074, 38.8298],
+      zoom: 8,
+    });
+
     init();
   }, []);
 
@@ -65,9 +60,7 @@ const Map = (props) => {
       ref={mapContainer}
       style={{ height: "100vh" }}
       className="map-container"
-    >
-      
-    </div>
+    />
   );
 };
 
